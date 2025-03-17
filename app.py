fdd50fe296e7d353f04ff3f47479429544e3e8a8
Ades <37319154+digitalfarmer@users.noreply.github.com>
2025-03-17 15:58:25 +0700

    update sample question

--- app.py
+++ app.py
@@ -103,11 +103,8 @@ st.subheader("RAG with bigquery")
 with st.expander("Sample prompts", expanded=True):
     st.write(
         """
-        - What kind of information is in this database?
-        - What percentage of orders are returned?
-        - How is inventory distributed across our regional distribution centers?
-        - Do customers typically place more than one order?
-        - Which product categories have the highest profit margins?
+        - Top 10 sales by netto?
+        
     """
     )
 
